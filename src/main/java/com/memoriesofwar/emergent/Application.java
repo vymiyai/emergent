eada9a7965f815ab6b40ce15d0db1b1a088f4645
vymiyai <vymiyai@gmail.com>
2017-12-17 20:07:51 +0100

    Created basic Spring application.

---
+++ src/main/java/com/memoriesofwar/emergent/Application.java
@@ -0,0 +1,67 @@
+package com.memoriesofwar;
+
+import com.sun.corba.se.impl.activation.CommandHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import sx.blah.discord.api.ClientBuilder;
+import sx.blah.discord.api.IDiscordClient;
+
+@SpringBootApplication
+public class Application {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    // as of 23.06.2017, moving the autowired annotation from the setter to this
+    // property fucks up everything.
+    private static CommandHandler commandHandler;
+
+    @Autowired
+    private void setCommandHandler(CommandHandler ch) {
+        commandHandler = ch;
+    }
+
+    private static String clientToken;
+
+    @Value("${discord.CLIENT_TOKEN}")
+    private void setClientToken(String ct) {
+        clientToken = ct;
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+
+        try {
+            IDiscordClient cli = new ClientBuilder().withToken(clientToken).withRecommendedShardCount().build();
+
+            cli.getDispatcher().registerListener(commandHandler);
+
+            // Only login after all events are registered otherwise some may be
+            // missed.
+            cli.login();
+        } catch (Exception e) {
+            // do nothing.
+            log.warn("WARNING - Discord4J :" + e.getMessage());
+        }
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run() throws Exception {
+        return args -> {
+            log.info("Application is now online.");
+        };
+    }
+}
